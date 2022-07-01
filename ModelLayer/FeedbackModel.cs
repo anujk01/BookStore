c1af2e6a2334eb6cdf68e855c5b25f12197cbf0f
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-07-01 15:15:35 +0530

    [Anuj]Adding: BookStore

---
+++ ModelLayer/FeedbackModel.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class FeedbackModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string FeedbackID { get; set; }
+
+        [ForeignKey("RegisterModel")]
+        public string userID { get; set; }
+        public virtual RegisterModel registerModel { get; set; }
+
+        [ForeignKey("BookModel")]
+        public string BookID { get; set; }
+        public virtual BookModel bookModel { get; set; }
+
+        [ForeignKey("Feedback")]
+        public string AddComment { get; set; }
+        public string Rating { get; set; }
+    }
+}
